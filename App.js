5c9cf5a51bd19e18ac2ae0fbead01d45ab29dc44
gittensjr25 <139085141+gittensjr25@users.noreply.github.com>
2025-11-03 19:17:30 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Reserve from "./pages/Reserve";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/reserve" element={<Reserve />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
